a9aba6cc436f09fe671942a18b992663745d6ff2
Sarafanoff Dmitriy <d.sarafanoff@gmail.com>
2020-03-24 15:43:31 +0100

    Master Commit 1 - Project initialized.

---
+++ src/GismeteoMobileApp.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using GismeteoMobileApp.Core.ViewModels.Home;
+
+namespace GismeteoMobileApp.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ src/GismeteoMobileApp.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.ViewModels;
+
+namespace GismeteoMobileApp.Core.ViewModels
+{
+    public abstract class BaseViewModel : MvxViewModel
+    {
+    }
+}
